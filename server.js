bbdf1a920280205b38367efc5e7f887757d95411
patriciamendoncaferraz <patricia.mendonca.ferraz@gmail.com>
2026-04-07 15:22:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -392,7 +392,7 @@ p{font-size:15px;color:#64748b;line-height:1.7;margin-bottom:8px}
 }
 </script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-1803618151"></script>
-<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','AW-1803618151');gtag('event','conversion',{'send_to':'AW-1803618151/purchase','value':47.0,'currency':'EUR'});</script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','AW-1803618151');gtag('event','conversion',{'send_to':'AW-1803618151/qawCCN7xyJccEI-UqZhD','value':47.0,'currency':'EUR'});
 </head>
 <body>
 <div class="card">
