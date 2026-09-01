ebc24b9f8e3cbdefcad3b6f2bdbb3a80f51f0fe4
patriciamendoncaferraz <patricia.mendonca.ferraz@gmail.com>
2026-09-01 17:57:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -693,7 +693,7 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '2595831900877757');
 fbq('track', 'PageView');
-${purchaseValue ? `fbq('track', 'Purchase', {value: ${purchaseValue}, currency: 'EUR'});` : `fbq('track', 'Purchase');`}
+fbq('track', 'Purchase', {value: ${purchaseValue || '0.00'}, currency: 'EUR'});
 </script>
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=2595831900877757&ev=PageView&noscript=1"/></noscript>
